9d2883a3beee9881ce9254b4e1290e525e099a03
anuragsuman <anuragsuman26@gmail.com>
2022-07-02 11:35:30 +0530

    AOP Java Project

---
+++ src/main/java/com/javatpoint/aspect/EmployeeServiceAspect.java
@@ -0,0 +1,45 @@
+package com.javatpoint.aspect;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.javatpoint.model.Employee;
+import com.javatpoint.model.Error;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class EmployeeServiceAspect {
+
+	@Autowired
+	private HttpServletRequest httpServletRequest;
+
+	@Pointcut(value= "execution(* com.javatpoint.service.EmployeeService.*(..)) and args(empId, fname, sname)")
+	private void logDisplayingBalance() throws Exception {
+	}
+
+	@Around("logDisplayingBalance()")
+	public Object around(ProceedingJoinPoint jp) throws Throwable{
+		String headerName = httpServletRequest.getHeader("access_token");
+		if(!headerName.equalsIgnoreCase("123")){
+			System.out.println("1232");
+			System.out.println(jp.proceed().getClass().getCanonicalName());
+			Error error = new Error();
+			error.setErrorDesc("token is not valid");
+			return error;
+		}
+		return  null;
+	}
+
+
+}
---
+++ src/main/java/com/javatpoint/model/Error.java
@@ -0,0 +1,14 @@
+package com.javatpoint.model;
+
+
+public class Error {
+    private String errorDesc;
+
+    public String getErrorDesc() {
+        return errorDesc;
+    }
+
+    public void setErrorDesc(String errorDesc) {
+        this.errorDesc = errorDesc;
+    }
+}
