41f60cc3f25a8a96803d1d99843545eb49c73579
anuragsuman <anuragsuman26@gmail.com>
2022-07-27 11:43:10 +0530

    tasklet job

---
+++ src/main/java/com/javatpoint/service/Day0Runnable.java
@@ -0,0 +1,10 @@
+package com.javatpoint.service;
+
+public class Day0Runnable implements Runnable{
+
+    @Override
+    public void run() {
+        // get the data from database
+        //all the logic needs to be write here
+    }
+}
---
+++ src/main/java/com/javatpoint/service/TaskSchedulingService.java
@@ -0,0 +1,32 @@
+package com.javatpoint.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.TaskScheduler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ScheduledFuture;
+
+public class TaskSchedulingService {
+
+    @Autowired
+    private TaskScheduler taskScheduler;
+
+    final long delayTime = 1000;
+
+    Map<String, ScheduledFuture<?>> jobsMap = new HashMap<>();
+
+    public void scheduleDayZero(String jobId, Runnable tasklet, String cronExpression){
+        ScheduledFuture<?> scheduledTask = taskScheduler.scheduleAtFixedRate(tasklet, delayTime);
+        jobsMap.put(jobId, scheduledTask);
+    }
+
+    public void removeScheduledTask(String jobId) {
+        ScheduledFuture<?> scheduledTask = jobsMap.get(jobId);
+        if (scheduledTask != null) {
+            scheduledTask.cancel(true);
+            jobsMap.put(jobId, null);
+        }
+    }
+
+}
